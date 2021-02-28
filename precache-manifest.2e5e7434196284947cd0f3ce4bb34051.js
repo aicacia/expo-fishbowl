1334a8b337230b6dcc13c4f7015f4db2056715f0
nathanfaucett <nathanfaucett@users.noreply.github.com>
2021-02-28 14:29:14 +0000

    deploy: 7696978e3ea1eda9cbd2366d7f9f57a604208fc5

---
+++ precache-manifest.2e5e7434196284947cd0f3ce4bb34051.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aa77f09218b10d36050",
+    "url": "/expo-fishbowl/static/js/0.1185501f.chunk.js"
+  },
+  {
+    "revision": "e3d24f5c9602e3b9af8c",
+    "url": "/expo-fishbowl/static/js/1.9c2148ec.chunk.js"
+  },
+  {
+    "revision": "5b9e8b524be2dcf38e3f",
+    "url": "/expo-fishbowl/static/js/app.a531ca34.chunk.js"
+  },
+  {
+    "revision": "75437b624ec6650ce0ba",
+    "url": "/expo-fishbowl/static/js/runtime~app.2ed0e524.js"
+  },
+  {
+    "revision": "442e32c55ba0abc4ccdd",
+    "url": "/expo-fishbowl/static/js/4.35ee9384.chunk.js"
+  },
+  {
+    "revision": "6df04570482c9511af74",
+    "url": "/expo-fishbowl/static/js/5.8af8c9aa.chunk.js"
+  },
+  {
+    "revision": "dcae99bdf3f930929d3f",
+    "url": "/expo-fishbowl/static/js/6.4a71d437.chunk.js"
+  },
+  {
+    "revision": "71830af4721ebafc6e44",
+    "url": "/expo-fishbowl/static/js/7.a80914e2.chunk.js"
+  },
+  {
+    "revision": "cb3da3b55a3efe3d8510583db14295e1",
+    "url": "/expo-fishbowl/404.html"
+  },
+  {
+    "revision": "e7e43576e941d37386c294f302e6ebf6",
+    "url": "/expo-fishbowl/index.html"
+  },
+  {
+    "revision": "733ae41490885452c1fb6b9a46a47020",
+    "url": "/expo-fishbowl/manifest.json"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/expo-fishbowl/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-fishbowl/expo-service-worker.js"
+  },
+  {
+    "revision": "92e55428a1ec8abaaa87664585e520cd",
+    "url": "/expo-fishbowl/register-service-worker.js"
+  },
+  {
+    "revision": "94457fbd6aa59ababd69e7abd584e579",
+    "url": "/expo-fishbowl/static/js/4.35ee9384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/expo-fishbowl/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/expo-fishbowl/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/expo-fishbowl/favicon.ico"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/expo-fishbowl/expo-service-worker.js",{scope:"/expo-fishbowl/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
